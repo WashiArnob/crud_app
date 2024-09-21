5f29ac30fe840a611f8412a32509a8f9af7d364b
WashiArnob <abuwashi.arnob7275924@gmail.com>
2024-09-21 22:35:04 +0600

    3rd commit

--- lib/api_service.dart
+++ lib/api_service.dart
@@ -2,33 +2,37 @@ import 'dart:convert';
 import 'package:crud_app/models/product.dart';
 import 'package:http/http.dart' as http;
 
-
 class ApiService {
-  final String baseUrl = '';
-
   // API Endpoints
-  final String fetchProductsUrl = '${'http://164.68.107.70:6060/api/v1/ReadProduct'}ReadProduct';
-  final String createProductUrl = '${'http://164.68.107.70:6060/api/v1/CreateProduct'}CreateProduct';
+  final Uri fetchProductsUrl = Uri.parse('http://164.68.107.70:6060/api/v1/ReadProduct');
+  final Uri createProductUrl = Uri.parse('http://164.68.107.70:6060/api/v1/CreateProduct');
+  final String baseUrl = 'http://164.68.107.70:6060/api/v1/';
 
-  // Update and Delete URLs are constructed with methods
-  String updateProductUrl(String id) => '${'http://164.68.107.70:6060/api/v1/UpdateProduct/6395ce12187245c05d68da82'}UpdateProduct/$id';
-  String deleteProductUrl(String id) => '${'http://164.68.107.70:6060/api/v1/DeleteProduct/639da5960817590a4e4fd53c'}DeleteProduct/$id';
+  // Update and Delete URLs
+  Uri updateProductUrl(String id) => Uri.parse('$baseUrl/UpdateProduct/$id');
+  Uri deleteProductUrl(String id) => Uri.parse('$baseUrl/DeleteProduct/$id');
 
   // Fetch Products
   Future<List<Product>> getProducts() async {
-    final response = await http.get(Uri.parse(fetchProductsUrl));
+    final response = await http.get(fetchProductsUrl);
     if (response.statusCode == 200) {
-      List<dynamic> body = jsonDecode(response.body);
-      return body.map((dynamic item) => Product.fromJson(item)).toList();
+      Map<String, dynamic> body = jsonDecode(response.body);
+
+      if (body['products'] != null) {
+        List<dynamic> productList = body['products'];
+        return productList.map((dynamic item) => Product.fromJson(item)).toList();
+      } else {
+        throw Exception("No products found in the response");
+      }
     } else {
-      throw Exception("Failed to load products");
+      throw Exception("Failed to load products: ${response.statusCode}");
     }
   }
 
   // Create a Product
   Future<Product> createProduct(Product product) async {
     final response = await http.post(
-      Uri.parse(createProductUrl),
+      createProductUrl,
       headers: {"Content-Type": "application/json"},
       body: jsonEncode(product.toJson()),
     );
@@ -42,7 +46,7 @@ class ApiService {
   // Update a Product
   Future<Product> updateProduct(String id, Product product) async {
     final response = await http.put(
-      Uri.parse(updateProductUrl(id)),
+      updateProductUrl(id),
       headers: {"Content-Type": "application/json"},
       body: jsonEncode(product.toJson()),
     );
@@ -55,7 +59,7 @@ class ApiService {
 
   // Delete a Product
   Future<void> deleteProduct(String id) async {
-    final response = await http.delete(Uri.parse(deleteProductUrl(id)));
+    final response = await http.delete(deleteProductUrl(id));
     if (response.statusCode != 200) {
       throw Exception("Failed to delete product");
     }
